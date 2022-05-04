902f1ac8342818d81d004c12cc73edff68f17b5e
adamowada <adamowada@gmail.com>
2022-05-04 08:14:26 -0700

    finished removing crispy forms, reset requirements.txt, installed dotenv, moved secret key to .env file, tested runserver

--- unicorn_ranch/unicorn_ranch/settings.py
+++ unicorn_ranch/unicorn_ranch/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wh8ac&)(mt!rg)cc79z!kr7ifw5776d5-85*b(rk@8*qjxxo-q'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,7 +46,6 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -139,5 +142,4 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# crispy form template-pack
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
