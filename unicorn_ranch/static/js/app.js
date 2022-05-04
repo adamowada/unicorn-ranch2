46554d92c715adbecb280e3ab1eb068ab3b090e6
adamowada <adamowada@gmail.com>
2022-05-04 09:12:03 -0700

    removed extra console logs

--- unicorn_ranch/static/js/app.js
+++ unicorn_ranch/static/js/app.js
@@ -1,13 +1,5 @@
 "use strict";
 
-console.log("proof of life");
-
-// remove before deployment
-$("#testButton").on("click", showLife);
-function showLife(event) {
-  console.log("jquery is working");
-}
-
 $(".dropDownButton").on("click", dropDownList);
 $(".cancel").on("click", cancelEdit);
 $(".dropDownList").submit(editLocation);
@@ -26,10 +18,7 @@ function editLocation(event) {
   event.preventDefault();
   const serializedData = $(this).serialize();
   const regex1 = /[^\r\n.]+=[^\r\n.]+=(\d+)/;
-  const regex2 = /[^\r\n.]+=[^\r\n.]+=[^\r\n.]+=([a-zA-Z]+)/;
   const id = serializedData.match(regex1)[1];
-  const location = serializedData.match(regex2)[1];
-  console.log(id, location); // flag to remove
   $.ajax({
     url: `unicorn-update/${id}`,
     type: "POST",
