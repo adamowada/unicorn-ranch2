0bd3e68a00d2af3bcd55c540a02205171bfb6de8
adamowada <adamowada@gmail.com>
2022-05-04 04:05:50 -0700

    downloaded and added edit icon next to change location

--- unicorn_ranch/staticfiles/js/app.js
+++ unicorn_ranch/staticfiles/js/app.js
@@ -1,3 +1,9 @@
-'use strict';
+"use strict";
 
-console.log('proof of life');
+console.log("proof of life");
+
+// remove before deployment
+$("#testButton").on("click", showLife);
+function showLife(event) {
+  console.log("jquery is working");
+}
