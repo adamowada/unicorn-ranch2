3ffa3031eae237bfd2488e23f692a8630dffc68e
adamowada <adamowada@gmail.com>
2022-05-04 08:02:51 -0700

    final makemigrations migrate and collectstatic

--- unicorn_ranch/staticfiles/js/app.js
+++ unicorn_ranch/staticfiles/js/app.js
@@ -7,3 +7,37 @@ $("#testButton").on("click", showLife);
 function showLife(event) {
   console.log("jquery is working");
 }
+
+$(".dropDownButton").on("click", dropDownList);
+$(".cancel").on("click", cancelEdit);
+$(".dropDownList").submit(editLocation);
+
+function dropDownList(event) {
+  $(this).hide();
+  $(this).next().show();
+}
+
+function cancelEdit(event) {
+  $(this).parent().parent().hide();
+  $(this).parent().parent().prev().show();
+}
+
+function editLocation(event) {
+  event.preventDefault();
+  const serializedData = $(this).serialize();
+  const regex1 = /[^\r\n.]+=[^\r\n.]+=(\d+)/;
+  const regex2 = /[^\r\n.]+=[^\r\n.]+=[^\r\n.]+=([a-zA-Z]+)/;
+  const id = serializedData.match(regex1)[1];
+  const location = serializedData.match(regex2)[1];
+  console.log(id, location); // flag to remove
+  $.ajax({
+    url: `unicorn-update/${id}`,
+    type: "POST",
+    data: serializedData,
+    dataType: "json",
+    success: function (data) {
+      console.log("it posted");
+      window.location.reload();
+    },
+  });
+}
