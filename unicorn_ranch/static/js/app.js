1698a76497213e5a3f5a4fead934a3b718c2b394
adamowada <adamowada@gmail.com>
2022-05-04 04:10:32 -0700

    added dropdown list with locations and jquery functions to hide

--- unicorn_ranch/static/js/app.js
+++ unicorn_ranch/static/js/app.js
@@ -7,3 +7,17 @@ $("#testButton").on("click", showLife);
 function showLife(event) {
   console.log("jquery is working");
 }
+
+
+$(".dropDownButton").on("click", dropDownList);
+$(".cancel").on("click", cancelEdit);
+
+function dropDownList(event) {
+    $(this).hide();
+    $(this).next().show();
+}
+
+function cancelEdit(event) {
+    $(this).parent().hide();
+    $(this).parent().prev().show();
+}
